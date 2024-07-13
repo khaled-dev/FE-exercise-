1b62b0bd1aa9bdbc55c946588395322f14988005
Khaled AbdElRahim <khaled710017@gmail.com>
2024-07-13 22:27:30 +0300

    WIP

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     const [customers, setCustomers] = useState([]);
     const [transactions, setTransactions] = useState([]);
     const [selectedCustomerId, setSelectedCustomerId] = useState(null);
+    const [selectedAmount, setSelectedAmount] = useState(null);
 
     useEffect(() => {
         axios.get('http://localhost:8080/customers')
@@ -23,12 +24,18 @@ function App() {
             .catch(error => console.error('Error fetching transactions:', error));
     }, []);
 
-    const filteredTransactions = transactions.filter(t => t.customer_id == selectedCustomerId);
+    const filteredTransactionsByName = transactions.filter(t => t.customer_id == selectedCustomerId);
+    const filteredTransactionsByAmount = transactions.filter(t => t.amount == selectedAmount);
 
     return (
         <div className="App">
-            <h1>Customer and Transaction Data</h1>
-                <CustomerTable customers={customers} transactions={transactions} />
+            <div>
+                <h1>List Customers and Transactions</h1>
+                <CustomerTable customers={customers} transactions={transactions}/>
+            </div>
+
+            <div>
+                <h1>List Transaction By user</h1>
                 <select onChange={(e) => setSelectedCustomerId(e.target.value)}>
                     <option value="">Select a Customer</option>
                     {customers.map(customer => (
@@ -37,10 +44,17 @@ function App() {
                 </select>
                 {selectedCustomerId && (
                     <>
-                    <TransactionList transactions={filteredTransactions} />
-                    <TransactionGraph transactions={filteredTransactions} />
-                </>
+                        <TransactionList transactions={filteredTransactionsByName}/>
+                        <TransactionGraph transactions={filteredTransactionsByName}/>
+                    </>
                 )}
+            </div>
+
+            <div>
+                <h1>List Transaction By user</h1>
+                <input type={"text"} onChange={(e) => setSelectedAmount(e.target.value)}/>
+                <TransactionList transactions={filteredTransactionsByAmount}/>
+            </div>
         </div>
     );
 }
